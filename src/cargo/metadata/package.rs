f0a6adc6814df71855ee285faeadd9cfdc33335d
MaulingMonkey <git@maulingmonkey.com>
2020-10-27 08:27:39 -0700

    cargo::metadata::{Package, Workspace}:  Methodify access
    - I want an unconditional Package::directory()
    - Modifyable paths could cause Package::directory() to assert
    - Mixing methods and members is awkward

--- src/cargo/metadata/package.rs
+++ src/cargo/metadata/package.rs
@@ -1,6 +1,6 @@
 use crate::cargo::toml;
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::ops::Deref;
 
 
@@ -9,8 +9,19 @@ use std::ops::Deref;
 #[derive(Debug)]
 #[non_exhaustive]
 pub struct Package<PackageMetadata = ::toml::value::Table> {
-    pub path: PathBuf,
-    pub toml: toml::Cargo<toml::Package<PackageMetadata>, ()>,
+    pub(crate) path: PathBuf,
+    pub(crate) toml: toml::Cargo<toml::Package<PackageMetadata>, ()>,
+}
+
+impl<PM> Package<PM> {
+    /// Path to the `Cargo.toml` this `[package]` was parsed out of
+    pub fn manifest_path(&self) -> &Path { &self.path }
+
+    /// Path to the directory containing the `Cargo.toml` this `[package]` was parsed out of
+    pub fn directory(&self) -> &Path { self.path.parent().unwrap() }
+
+    /// Parsed `Cargo.toml` contents (also available via [Deref])
+    pub fn toml(&self) -> &toml::Cargo<toml::Package<PM>, ()> { &self.toml }
 }
 
 impl<PM> Deref for Package<PM> {
--- src/cargo/metadata/workspace.rs
+++ src/cargo/metadata/workspace.rs
@@ -1,16 +1,24 @@
 use crate::cargo::toml;
 
 use std::ops::Deref;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 
 
-/// A workspace directory, and possibly a corresponding `[workspace]`
+/// A workspace [directory](Workspace::directory), and corresponding <code>\[[workspace](toml::Workspace)\]</code>
 #[derive(Debug, Default)]
 #[non_exhaustive]
 pub struct Workspace<WorkspaceMetadata = ::toml::value::Table> {
-    pub directory:  PathBuf,
-    pub toml:       toml::Workspace<WorkspaceMetadata>,
+    pub(crate) directory:  PathBuf,
+    pub(crate) toml:       toml::Workspace<WorkspaceMetadata>,
+}
+
+impl <WM> Workspace<WM> {
+    /// Path to the directory containing the `Cargo.toml` this <code>\[[workspace](toml::Workspace)\]</code> was parsed out of
+    pub fn directory(&self) -> &Path { &self.directory }
+
+    /// Parsed `Cargo.toml` contents (also available via [Deref])
+    pub fn toml(&self) -> &toml::Workspace<WM> { &self.toml }
 }
 
 impl<WM> Deref for Workspace<WM> {
