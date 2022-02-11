3d97101ccfe1f0daba045d367fbeb7740547272e
MaulingMonkey <git@maulingmonkey.com>
2022-02-11 10:23:19 -0800

    path.has_extension[_ignore_ascii_case]

--- src/path.rs
+++ src/path.rs
@@ -1,5 +1,6 @@
 //! Similar to [std::path::*](std::path)
 
+use std::ffi::OsStr;
 use std::path::{Component, Path, PathBuf, Prefix};
 
 /// Cleanup/simplify path as much as possible
@@ -61,4 +62,122 @@ pub fn cleanup(path: impl AsRef<Path>) -> PathBuf {
         p.push(".");
     }
     p
+}
+
+/// Check the extension of a file
+///
+/// # Examples
+///
+/// ```rust
+/// # use mmrbi::path::*;
+/// // The full filename doesn't count as an extension
+/// assert!(!has_extension("foo.tar.gz", ".foo.tar.gz"));
+/// assert!(!has_extension("foo.tar.gz",  "foo.tar.gz"));
+/// assert!(!has_extension("foo.tar.gz",   "oo.tar.gz"));
+///
+/// // Only the full extension (with or without a leading `.`) counts
+/// assert!( has_extension("foo.tar.gz", ".tar.gz"));
+/// assert!( has_extension("foo.tar.gz",  "tar.gz"));
+/// assert!(!has_extension("foo.tar.gz",   "ar.gz"));
+///
+/// // Case sensitive
+/// assert!(!has_extension("foo.tAr.gZ", ".tar.gz"));
+/// assert!(!has_extension("foo.tAr.gZ",  "tar.gz"));
+/// assert!(!has_extension("foo.tAr.gZ",   "ar.gz"));
+///
+/// assert!( has_extension("foo.tar.gz", ".gz"));
+/// assert!( has_extension("foo.tar.gz",  "gz"));
+/// assert!(!has_extension("foo.tar.gz",   "z"));
+/// ```
+pub fn has_extension(path: impl AsRef<Path>, ext: impl AsRef<OsStr>) -> bool {
+    has_extension_impl(path, ext, |a,b| a.eq(b))
+}
+
+/// Check the extension of a file
+///
+/// # Examples
+///
+/// ```rust
+/// # use mmrbi::path::*;
+/// // The full filename doesn't count as an extension
+/// assert!(!has_extension_ignore_ascii_case("foo.tar.gz", ".foo.tar.gz"));
+/// assert!(!has_extension_ignore_ascii_case("foo.tar.gz",  "foo.tar.gz"));
+/// assert!(!has_extension_ignore_ascii_case("foo.tar.gz",   "oo.tar.gz"));
+///
+/// // Only the full extension (with or without a leading `.`) counts
+/// assert!( has_extension_ignore_ascii_case("foo.tar.gz", ".tar.gz"));
+/// assert!( has_extension_ignore_ascii_case("foo.tar.gz",  "tar.gz"));
+/// assert!(!has_extension_ignore_ascii_case("foo.tar.gz",   "ar.gz"));
+///
+/// // Case insensitive
+/// assert!( has_extension_ignore_ascii_case("foo.tAr.gZ", ".tar.gz"));
+/// assert!( has_extension_ignore_ascii_case("foo.tAr.gZ",  "tar.gz"));
+/// assert!(!has_extension_ignore_ascii_case("foo.tAr.gZ",   "ar.gz"));
+///
+/// assert!( has_extension_ignore_ascii_case("foo.tar.gz", ".gz"));
+/// assert!( has_extension_ignore_ascii_case("foo.tar.gz",  "gz"));
+/// assert!(!has_extension_ignore_ascii_case("foo.tar.gz",   "z"));
+/// ```
+pub fn has_extension_ignore_ascii_case(path: impl AsRef<Path>, ext: impl AsRef<OsStr>) -> bool {
+    has_extension_impl(path, ext, |a,b| a.eq_ignore_ascii_case(b))
+}
+
+fn has_extension_impl(path: impl AsRef<Path>, ext: impl AsRef<OsStr>, eq: impl FnOnce(&[u8], &[u8]) -> bool) -> bool {
+    let ext = os_str_to_likely_wtf8_bytes(ext.as_ref());
+    if ext.is_empty() { return true }
+    let ext = ext.strip_prefix(b".").unwrap_or(ext);
+    // ext = "tar.gz"
+
+    let file_name = match path.as_ref().file_name() {
+        None => return false,
+        Some(p) => os_str_to_likely_wtf8_bytes(p),
+    };
+    // file_name = "foo.tar.gz"
+
+    if file_name.len() <= ext.len() {
+        // file_name is either smaller than ext, or doesn't have enough space for the leading `.`
+        // (might be exactly equal to `ext`, but that's not the same thing as *having* `ext` as an extension IMO)
+        false
+    } else if let Some(file_ext) = file_name[file_name.len() - ext.len() - 1..].strip_prefix(b".") {
+        eq(file_ext, ext)
+    } else { // `ext` was not preceeded by `.`
+        false
+    }
+}
+
+#[cfg(windows)] fn is_os_str_to_bytes_transmute_safe_probably() -> bool {
+    #[repr(C)] struct Sliceish { ptr: usize, len: usize }
+
+    let astr = "a";
+    let a = OsStr::new(astr);
+    if a.len() != 1 { return false }
+    // SAFETY:
+    //  ✔️ all bit patterns of Sliceish should be valid, no invariants to violate
+    //  ⚠️ if &OsStr ever has padding, this would be an unsound read of potentially uninitialized data
+    let a : Sliceish = unsafe { std::mem::transmute(a) };
+    if a.len != 1 { return false }
+    if a.ptr != astr.as_ptr() as usize { return false }
+
+    true
+}
+
+#[cfg(windows)] #[test] fn check_transmute_safe() {
+    assert!(is_os_str_to_bytes_transmute_safe_probably());
+}
+
+fn os_str_to_likely_wtf8_bytes<'s>(os: &'s (impl AsRef<OsStr> + ?Sized)) -> &'s [u8] {
+    return imp(os.as_ref());
+
+    #[cfg(unix)]
+    fn imp(os: &OsStr) -> &[u8] {
+        use std::os::unix::ffi::*;
+        os.as_bytes()
+    }
+
+    #[cfg(windows)]
+    fn imp(os: &OsStr) -> &[u8] {
+        assert!(is_os_str_to_bytes_transmute_safe_probably());
+        // UNSAFETY: ❌ technically super unsound?
+        unsafe { std::mem::transmute(os) }
+    }
 }
--- src/path_ext.rs
+++ src/path_ext.rs
@@ -1,3 +1,4 @@
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 
 /// Utility methods for [std::path::Path](std::path::Path)\[[Buf](std::path::PathBuf)\]
@@ -28,6 +29,64 @@ pub trait PathExt : AsRef<Path> {
     /// }
     /// ```
     fn cleanup(&self) -> PathBuf { crate::path::cleanup(self) }
+
+    /// Check the extension of a file
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use std::path::Path;
+    /// use mmrbi::PathExt;
+    ///
+    /// // The full filename doesn't count as an extension
+    /// assert!(!Path::new("foo.tar.gz").has_extension(".foo.tar.gz"));
+    /// assert!(!Path::new("foo.tar.gz").has_extension( "foo.tar.gz"));
+    /// assert!(!Path::new("foo.tar.gz").has_extension(  "oo.tar.gz"));
+    ///
+    /// // Only the full extension (with or without a leading `.`) counts
+    /// assert!( Path::new("foo.tar.gz").has_extension(".tar.gz"));
+    /// assert!( Path::new("foo.tar.gz").has_extension( "tar.gz"));
+    /// assert!(!Path::new("foo.tar.gz").has_extension(  "ar.gz"));
+    ///
+    /// // Case sensitive
+    /// assert!(!Path::new("foo.tAr.gZ").has_extension(".tar.gz"));
+    /// assert!(!Path::new("foo.tAr.gZ").has_extension( "tar.gz"));
+    /// assert!(!Path::new("foo.tAr.gZ").has_extension(  "ar.gz"));
+    ///
+    /// assert!( Path::new("foo.tar.gz").has_extension(".gz"));
+    /// assert!( Path::new("foo.tar.gz").has_extension( "gz"));
+    /// assert!(!Path::new("foo.tar.gz").has_extension(  "z"));
+    /// ```
+    fn has_extension(&self, ext: impl AsRef<OsStr>) -> bool { crate::path::has_extension(self, ext) }
+
+    /// Check the extension of a file
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use std::path::Path;
+    /// use mmrbi::PathExt;
+    ///
+    /// // The full filename doesn't count as an extension
+    /// assert!(!Path::new("foo.tar.gz").has_extension_ignore_ascii_case(".foo.tar.gz"));
+    /// assert!(!Path::new("foo.tar.gz").has_extension_ignore_ascii_case( "foo.tar.gz"));
+    /// assert!(!Path::new("foo.tar.gz").has_extension_ignore_ascii_case(  "oo.tar.gz"));
+    ///
+    /// // Only the full extension (with or without a leading `.`) counts
+    /// assert!( Path::new("foo.tar.gz").has_extension_ignore_ascii_case(".tar.gz"));
+    /// assert!( Path::new("foo.tar.gz").has_extension_ignore_ascii_case( "tar.gz"));
+    /// assert!(!Path::new("foo.tar.gz").has_extension_ignore_ascii_case(  "ar.gz"));
+    ///
+    /// // Case insensitive
+    /// assert!( Path::new("foo.tAr.gZ").has_extension_ignore_ascii_case(".tar.gz"));
+    /// assert!( Path::new("foo.tAr.gZ").has_extension_ignore_ascii_case( "tar.gz"));
+    /// assert!(!Path::new("foo.tAr.gZ").has_extension_ignore_ascii_case(  "ar.gz"));
+    ///
+    /// assert!( Path::new("foo.tar.gz").has_extension_ignore_ascii_case(".gz"));
+    /// assert!( Path::new("foo.tar.gz").has_extension_ignore_ascii_case( "gz"));
+    /// assert!(!Path::new("foo.tar.gz").has_extension_ignore_ascii_case(  "z"));
+    /// ```
+    fn has_extension_ignore_ascii_case(&self, ext: impl AsRef<OsStr>) -> bool { crate::path::has_extension_ignore_ascii_case(self, ext) }
 }
 
 impl<P: AsRef<Path>> PathExt for P {}
