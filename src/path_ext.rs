d1b82867034cfd61df55f6872c0a4cda8dc1e9f5
MaulingMonkey <git@maulingmonkey.com>
2022-02-10 17:41:09 -0800

    PathExt : AsRef<Path>

--- src/path_ext.rs
+++ src/path_ext.rs
@@ -1,7 +1,7 @@
 use std::path::{Path, PathBuf};
 
 /// Utility methods for [std::path::Path](std::path::Path)\[[Buf](std::path::PathBuf)\]
-pub trait PathExt {
+pub trait PathExt : AsRef<Path> {
     /// Cleanup/simplify path as much as possible
     ///
     /// # Examples
@@ -9,13 +9,13 @@ pub trait PathExt {
     /// ```rust
     /// # use std::path::Path;
     /// use mmrbi::PathExt;
-    /// 
+    ///
     /// assert_eq!(Path::new("a/b").cleanup(),                      Path::new("a/b"));
     /// assert_eq!(Path::new("a/b/..").cleanup(),                   Path::new("a"));
     /// assert_eq!(Path::new("a/b/../..").cleanup(),                Path::new("."));
     /// assert_eq!(Path::new("a/b/../../..").cleanup(),             Path::new(".."));
     /// assert_eq!(Path::new("a/b/../../../..").cleanup(),          Path::new("../.."));
-    /// 
+    ///
     /// assert_eq!(Path::new("../../a/b").cleanup(),                Path::new("../../a/b"));
     /// assert_eq!(Path::new("../../a/b/..").cleanup(),             Path::new("../../a"));
     /// assert_eq!(Path::new("../../a/b/../..").cleanup(),          Path::new("../.."));
@@ -27,8 +27,7 @@ pub trait PathExt {
     ///     assert_eq!(Path::new(r"\\?\C:\foo\bar").cleanup(),      Path::new(r"C:\foo\bar"));
     /// }
     /// ```
-    fn cleanup(&self) -> PathBuf;
+    fn cleanup(&self) -> PathBuf { crate::path::cleanup(self) }
 }
 
-impl PathExt for &Path      { fn cleanup(&self) -> PathBuf { crate::path::cleanup(self) } }
-impl PathExt for PathBuf    { fn cleanup(&self) -> PathBuf { crate::path::cleanup(self) } }
+impl<P: AsRef<Path>> PathExt for P {}
